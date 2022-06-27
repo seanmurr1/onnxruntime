9589c68bb1f38ca81d8cd43d07ccbd8b88dd2bb6
Dmitri Smirnov <dmitrism@microsoft.com>
2022-06-27 11:57:24 -0700

    Refactor Tensor class

--- onnxruntime/core/framework/orttraining_partial_executor.h
+++ onnxruntime/core/framework/orttraining_partial_executor.h
@@ -24,8 +24,8 @@ class PartialExecutor : public IExecutor {
       : state_{state},
         cache_{cache} {}
 
-  common::Status Execute(const SessionState& session_state, const std::vector<int>& feed_mlvalue_idxs,
-                         const std::vector<OrtValue>& feeds, const std::vector<int>& fetch_mlvalue_idxs,
+  common::Status Execute(const SessionState& session_state, gsl::span<const int> feed_mlvalue_idxs,
+                         gsl::span<const OrtValue> feeds, gsl::span<const int> fetch_mlvalue_idxs,
                          std::vector<OrtValue>& fetches,
                          const std::unordered_map<size_t, CustomAllocator>& fetch_allocators,
                          const logging::Logger& logger) override;
